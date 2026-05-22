f67c38d7c1e5768620d25dea69e33b94ed1ffab7
n8n-markcid <n8n-markcid@users.noreply.github.com>
2026-05-22 04:14:06 +0000

    Supabase backup

--- prisma/backups/data.sql
+++ prisma/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict 7LWGgghIvg6ZlkjM0WnRbWRnWkXG1tuegcMsQbufY9Pg4JICpRDlfBKvFPwGArb
+-- \restrict sIQHZ14l6SHkvIh61RaA1uYaempJDydnQktbf4XHdcwKxkfaGpFV0N9uxReanQe
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.6
@@ -1860,6 +1860,6 @@ SELECT pg_catalog.setval('"public"."workflow_statistics_id_seq"', 28, true);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict 7LWGgghIvg6ZlkjM0WnRbWRnWkXG1tuegcMsQbufY9Pg4JICpRDlfBKvFPwGArb
+-- \unrestrict sIQHZ14l6SHkvIh61RaA1uYaempJDydnQktbf4XHdcwKxkfaGpFV0N9uxReanQe
 
 RESET ALL;
